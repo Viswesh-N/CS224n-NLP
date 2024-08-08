3544175f837b646a6dd262bea0bdb89af0730658
Viswesh N <nvichu1001@gmail.com>
2024-08-08 13:30:51 +0530

    added first two parts implementation and finetuning without pretrain

---
+++ Problem-sets-and-quizzes/a4/student/src/london_baseline.py
@@ -0,0 +1,25 @@
+# TODO: [part d]
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+
+import argparse
+import utils
+
+def main():
+    accuracy = 0.0
+
+    # Compute accuracy in the range [0.0, 100.0]
+    ### YOUR CODE HERE ###
+    total, correct = utils.evaluate_places('birth_dev.tsv', ['London'] * len([line for line in open('birth_dev.tsv').read().split('\n') if line]))
+    ### END YOUR CODE ###
+
+    accuracy = total*100/correct
+
+    return accuracy
+
+if __name__ == '__main__':
+    accuracy = main()
+    with open("london_baseline_accuracy.txt", "w", encoding="utf-8") as f:
+        f.write(f"{accuracy}\n")
